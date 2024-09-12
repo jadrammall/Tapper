d00916f7b5959dbaa9af6ded84541a361ecd5da8
jadrammall <jrammal12@gmail.com>
2024-09-12 22:04:07 +0300

    Final Commit

--- Tapper/GameViewController.swift
+++ Tapper/GameViewController.swift
@@ -5,4 +5,35 @@
 //  Created by Jad Rammal on 12/09/2024.
 //
 
-import Foundation
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    
+    var count: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tapButton.layer.cornerRadius = tapButton.frame.width/2
+        countLabel.text = "\(count!)"
+    }
+    
+    @IBAction func onTapButtonPress(_ sender: Any) {
+        count -= 1
+        countLabel.text = "\(count!)"
+        
+        if count == 0 {
+            tapButton.isEnabled = false
+            
+            let alert = UIAlertController(title: "Cancel", message: "You won", preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "Dismiss", style: .default)
+            alert.addAction(dismissAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+}
--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -16,38 +16,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         playButton.layer.cornerRadius = 12
-        self.hideKeyboardWhenTappedAround() 
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard tapsTextField.text != nil else {
-            return
-        }
-        let selectedNumber = Int(tapsTextField.text ?? "0")
-        let gameVC = segue.destination as! GameViewController
-        gameVC.count = selectedNumber
+        self.hideKeyboardWhenTappedAround()
     }
     
     @IBAction func onPlayButtonTap(_ sender: Any) {
         if let text = tapsTextField.text, let count = Int(text) {
-            play()
+            if count > 0 && count < 20 {
+                presentGameController(count: count)
+            } else {
+                showAlert(title: "Dismiss", message: "Number should be between 0 & 20")
+            }
         } else {
-            showAlert()
+            showAlert(title: "Error", message: "Please enter a valid number!")
         }
     }
     
-    func showAlert() {
-        let alert = UIAlertController(title: "Error", message: "Please enter a valid number!", preferredStyle: .alert)
-        
-        let dismissAction = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .default)
         alert.addAction(dismissAction)
         
         present(alert, animated: true, completion: nil)
     }
     
-    func play() {
+    func presentGameController(count: Int) {
+        // Step 1: Declare the storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
+        //Step 2: Declare the destination controller
+        let destination = storyboard.instantiateViewController(identifier: "GameViewController") as GameViewController
+        
+        //Step 3: Send the data
+        destination.count = count
+        
+        //Step 4: Present the destination
+        present(destination, animated: true)
     }
+    
 }
 
 extension UIViewController {
